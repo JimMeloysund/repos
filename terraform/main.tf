46b1206ecd7e528c1ec5bed52c4dd76224a57192
Jim Meløysund <jim_meloysund@hotmail.com>
2024-08-04 16:54:03 +0200

    merge

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,10 @@ resource "github_repository" "portfolio" {
   name        = "portfolio"
   description = "Website for my portfolio"
   visibility  = "public"
+
+  allow_merge_commit = true
+  allow_rebase_merge = true
+  allow_squash_merge = true
 }
 
 output "example_repo_clone_rul" {
