cd034657c7c74983d4633acf1976e73909d3e522
Jim Meløysund <jim_meloysund@hotmail.com>
2024-12-07 18:42:28 +0100

    Added broadcastserver

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,16 @@ resource "github_repository" "portfolio" {
   allow_squash_merge = true
 }
 
+resource "github_repository" "broadcast_server" {
+  name        = "broadcast-server"
+  description = "roadmap.sh broadcast server written in js with node.js"
+  visibility  = "public"
+
+  allow_merge_commit = true
+  allow_rebase_merge = true
+  allow_squash_merge = true
+}
+
 output "example_repo_clone_rul" {
   value = github_repository.portfolio.ssh_clone_url
 }
